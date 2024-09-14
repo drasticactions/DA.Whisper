5addf8c185714729fa804542c583647f5e44184f
Timothy Miller <innerlogic4321@gmail.com>
2024-09-15 00:54:22 +0900

    Update

--- src/DA.WhisperBlazor/Program.cs
+++ src/DA.WhisperBlazor/Program.cs
@@ -5,6 +5,27 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DA.WhisperBlazor;
+using DA.Whisper;
+
+var consoleLog = new ConsoleLog(false);
+WhisperLogger.Instance.OnLog += (e) =>
+{
+    switch (e.Level)
+    {
+        case DA.Whisper.LogLevel.Error:
+            consoleLog.LogError(e.Message);
+            break;
+        case DA.Whisper.LogLevel.Info:
+            consoleLog.Log(e.Message);
+            break;
+        case DA.Whisper.LogLevel.Warning:
+            consoleLog.LogWarning(e.Message);
+            break;
+        default:
+            consoleLog.LogDebug(e.Message);
+            break;
+    }
+};
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
